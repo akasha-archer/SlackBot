52c94bbf54699e67846c323934d8b43c0aa31689
cito125 <cito125@gmail.com>
2016-09-24 18:44:57 -0400

    Remove unneccesary comments from main

--- src/nyc/c4q/ramonaharrison/Main.java
+++ src/nyc/c4q/ramonaharrison/Main.java
@@ -5,25 +5,11 @@ import nyc.c4q.ramonaharrison.network.Slack;
 public class Main {
 
     public static void main(String[] args) {
-
         Bot myBot = new Bot();
-
         myBot.testApi();
-
-      //  myBot.listChannels();
-
-       myBot.listMessages(Slack.BOTS_CHANNEL_ID);
-
-        myBot.checkForWord();
+        myBot.listMessages(Slack.BOTS_CHANNEL_ID);
         myBot.keepCheckingForWord();
 
-      // myBot.getAnEvent();
-
-        // Post "Hello, world!" to the #bots channel
-       // myBot.sendMessageToBotsChannel("Hola, mundo! Como se escribe java en espanol??");
-
-        // Post a pineapple photo to the #bots channel
-       // myBot.sendMessageToBotsChannel("http://placehold.it/350x250");
 
     }
 }
